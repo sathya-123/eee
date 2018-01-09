c70f8b30d86b7dba05ea6795287c36a23bd0a7c0
sathya-123 <30828591+sathya-123@users.noreply.github.com>
2018-01-09 16:54:33 +0530

    jjj.c

---
+++ jjj.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+print("enter the character");
+scanf("%s",&c);
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
+printf("c is a vowel");
+else
+printf("c is a constant");
+}
