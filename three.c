bc27c87efd96bc9365579a8cd79c4397d66a990c
sathya-123 <30828591+sathya-123@users.noreply.github.com>
2018-01-11 16:14:07 +0530

    Update three.c

--- three.c
+++ three.c
@@ -1,4 +1,13 @@
 #include<stdio.h>
 void main()
 {
-if a,b,c;
+int a,b,c;
+printf("enter the numbers");
+scanf("%d %d %d",&a,&b,&c);
+if(a>b&&a>c)
+printf("a is greater);
+elseif(b>c)
+printf("b is greater");
+else
+printf("c is greater");
+}
