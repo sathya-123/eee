e3de09690465ce2064b12ca26bde91fcbd15b5f8
sathya-123 <30828591+sathya-123@users.noreply.github.com>
2018-01-11 16:44:25 +0530

    digits.c

---
+++ digits.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+long long n;
+int count=0;
+printf("enter the value");
+scanf("%lld",&n);
+while(n!=0)
+{
+n=n/10;
+++count;
+}
+printf9"number of digits in integer%d",count);
+}
