aa9c717e1fdf96e2b28bf1598a9ddf4dcac09869
sathya-123 <30828591+sathya-123@users.noreply.github.com>
2018-01-11 16:00:48 +0530

    alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+char c;
+printf("enter the character");
+scanf("%s",&c);
+if(c<=a&&c>=z||c<=A&&c>=Z)
+printf("it is alphabet");
+else
+printf("it is not a alphabet");
+}
