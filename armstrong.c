d332e2ef5c2d7d4e91f2e5a619b156b8d1fe93ba
sathya-123 <30828591+sathya-123@users.noreply.github.com>
2018-01-21 10:17:01 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
 void main()
 {
-int n,sum=0,rem=0;
+int n,sum=0,rem=0,cube;
 printf("enter a number");
 scanf("%d",&n);
 while(n!=0)
 {
 rem=n%10;
-cube=pow(rem,3);
+cube=rem*rem*rem;
 sum=sum+cube;
+ n=n/10;
 }
 if(sum==n)
 printf("its a armstrong %d");
 else
-printf("its not a armstrong %d);
+printf("its not a armstrong %d");
 }
