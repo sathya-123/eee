9981e20538032f6412e93eb4b2c5298a9b77f647
sathya-123 <30828591+sathya-123@users.noreply.github.com>
2018-01-21 11:24:13 +0530

    Update 20.c

--- 20.c
+++ 20.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-void main()
+int main()
 {
 int i,n,s=0;
 printf("enter the number");
@@ -7,6 +7,7 @@ scanf("%d",&n);
 for(i=0;i<=5;i++)
 {
 s=n*i;
-printf("the number is %d",s);
+printf("%d",s);
 }
+return 0;
 }
