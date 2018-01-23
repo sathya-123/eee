5e3bc6d33554c9036520ca7cd3b9f15ec75c8331
sathya-123 <30828591+sathya-123@users.noreply.github.com>
2018-01-23 16:55:38 +0530

    Create 23.c

---
+++ 23.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,a[30],i,mi;
+printf("enter the number");
+scanf("%d",&n);
+printf("enter the elements");
+scanf("%d",a[i]);
+min=a[i];
+for(i=0;i<n;i++)
+{
+if(a[i]>min)
+min=a[i];
+printf("min element",min);
+}
+}
