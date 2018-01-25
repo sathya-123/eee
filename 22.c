5065156dcb704d6ea8e61fb746667b8774be10fb
sathya-123 <30828591+sathya-123@users.noreply.github.com>
2018-01-25 16:12:03 +0530

    Update 22.c

--- 22.c
+++ 22.c
@@ -1,3 +1,16 @@
 #include<stdio.h>
 void main()
 {
+int n,a[30],ma,i;
+printf("enter the number');
+scanf("%d",&n);
+printf("enter the elements");
+scanf("%d",&a[i]);
+ma=a[i];
+for(i=0;i<n;i++)
+{
+if(a[i]<ma)
+ma=a[i];
+printf("max element',ma);
+}
+}
