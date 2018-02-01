3088a3595cdfb111c84d5830f2eea50f74126ef0
sathya-123 <30828591+sathya-123@users.noreply.github.com>
2018-02-01 10:46:48 +0000

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,k,a[10],i;
+printf("enter the value");
+scanf("%d %d",&n,&k);
+for(i=0;i<n;i++)
+{
+scanf("%d",%a[i]);
+}
+for(i=0;i<k;i++)
+{
+sum=sum+a[i];
+}
+printf("%d",sum
