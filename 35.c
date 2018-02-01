187a57b2dc2d4e3dd13b23b2bcfd422abcec5924
sathya-123 <30828591+sathya-123@users.noreply.github.com>
2018-02-01 10:29:44 +0000

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,count=0;
+printf("enter the number");
+scanf("%d",&a);
+while(n!=0)
+{
+rem=a%10;
+count++;
+a=a/10;
+}
+printf("the value is %d",a);
+return 0;
+}
