1a15e1f36ff29c3488934a02cb5e596917d3eaf5
sathya-123 <30828591+sathya-123@users.noreply.github.com>
2018-02-02 08:56:54 +0000

    Update 16.c

--- 16.c
+++ 16.c
@@ -3,4 +3,20 @@ int main()
 {
 int a1,a2,c;
 printf("enter the intervals");
-scanf
+scanf("%d %d",&a1,&a2);
+while(a1<a2)
+{
+  c=0;
+  for(i=2;i<a1%2;i++)
+  {
+    c=1;
+    break;
+  }
+}
+  if(c==0)
+  {
+    printf("%d",a1);
+    a1++;
+  }
+return 0;
+}
