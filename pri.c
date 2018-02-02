aede2672c2a0f46a32112f4524b1e64b105c6cc2
sathya-123 <30828591+sathya-123@users.noreply.github.com>
2018-02-02 08:41:23 +0000

    Update pri.c

--- pri.c
+++ pri.c
@@ -3,15 +3,19 @@ void main()
 {
 int i,n,c=0;
 printf("enter the number");
-scanf("%d",&a);
-for(i=1;i<=n;i++)
+scanf("%d",&n);
+for(i=2;i<=n/2;i++)
 {
 if(n%i==0)
-c++;
+c=1;
+  break;
 }
-}
-if(c==2)
+if(c==0)
+{
 printf("the number is prime");
+}
 else
-printf("the number is nopt a prime");
+{
+printf("the number is not a prime");
+}
 }
