62a404a9a2d46ee571059d67f3f1ea9ba89e9362
sathya-123 <30828591+sathya-123@users.noreply.github.com>
2018-02-08 09:06:59 +0000

    Create 31.c

---
+++ 31.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+char a[56];
+int b=0,c=0;
+printf("enter the para");
+scanf("%s",a);
+if(a==' ')
+{
+  b++;
+}
+  else
+  {
+    c++;
+    printf("%d",a);
+  }
+return 0;
+}
+
+
