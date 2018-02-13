289545889ad6d47df8c07a6b6f9a35feda56d152
sathya-123 <30828591+sathya-123@users.noreply.github.com>
2018-02-13 08:59:47 +0000

    Create 50 1.c

---
+++ 50 1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("yes it is a power of 2");
+}
+else
+{
+  printf("no it is not a power of 2");
+}
+return 0;
+}
