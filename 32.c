eca8a265b664d3308e77537cb6585a8301ed7f21
sathya-123 <30828591+sathya-123@users.noreply.github.com>
2018-02-27 15:50:49 +0530

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+  char a[50];
+  int i,b;
+  printf("enter the line");
+  scanf("%[^\n]",a);
+  for(i=0;a[i]!='\0';i++)
+  {
+    if(a[i]==' ')
+    {
+      b++;
+    }
+  }
+  printf("%d",b+1);
+}
