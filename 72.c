b590d83a8a48163ee3bdf7ba36b1cfeff409a367
sathya-123 <30828591+sathya-123@users.noreply.github.com>
2018-03-01 16:50:31 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  char a[80],i;
+  printf("enter the string");
+  scanf("%s",a);
+  if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u')
+{
+  printf("yes");
+}  
+else
+{
+  printf("no");
+}
+return 0;
+}
