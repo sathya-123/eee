1bce692e1e81b7d3d830571989436955075fe5de
sathya-123 <30828591+sathya-123@users.noreply.github.com>
2018-03-13 10:46:45 +0000

    Create 94.c

---
+++ 94.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() 
+{
+int kelvin;
+float celsius;
+printf("enter the celsius value\n");
+scanf("%f",&celsius);
+kelvin=celsius+273;
+printf("%d",kelvin);
+  return 0;
+}
